42ed5087c6e6251f98a4bfb5d295537cb940565e
Manojkumar886 <maddymano434@gmail.com>
2023-02-20 10:40:40 +0530

    initial commit

--- src/main/java/FundamentalJava/App.java
+++ src/main/java/FundamentalJava/App.java
@@ -1,9 +1,6 @@
 package FundamentalJava;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
--- src/main/java/FundamentalJava/sample.java
+++ src/main/java/FundamentalJava/sample.java
@@ -1,2 +1,13 @@
-package FundamentalJava;public class sample {
+package FundamentalJava;
+
+public class sample {
+    public static void main(String[] args) {
+        //System.out.println("Surendaran");  //single line comments
+        System.out.println("Annamalai");
+        /*System.out.println("ManojKumar");
+        System.out.println("Raja");*/  //Multiline comments
+        System.out.println("KUMAR");
+
+
+    }
 }
