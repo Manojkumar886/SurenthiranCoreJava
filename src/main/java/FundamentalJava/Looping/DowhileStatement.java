1c00a5bd2c15e81a30c768b594a52696972671f8
Manojkumar886 <maddymano434@gmail.com>
2023-03-06 12:50:00 +0530

    Looping is over

---
+++ src/main/java/FundamentalJava/Looping/DowhileStatement.java
@@ -0,0 +1,33 @@
+package FundamentalJava.Looping;
+
+import java.util.Scanner;
+
+public class DowhileStatement
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+
+        int staff=10;
+        float time=0;
+        int count=0;
+
+        do {
+
+            System.out.println(staff+" staff-enter login time");
+            time =scan.nextFloat();
+            if(time>9.15)
+            {
+                System.out.println(time+"your late comer so,your salary 10 % is deducted");
+                count++;
+            }
+            else {
+                System.out.println("your good time come to school");
+            }
+            staff--;
+
+        }
+        while(staff>0);
+        System.out.println("this month later commer count is "+count);
+    }
+}
